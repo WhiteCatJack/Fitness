c02bad7e95e10f8280e3b1bcbf3f1a980726438e
ZekunWei <zekunwei@outlook.com>
2019-03-19 14:02:39 +0800

    update

--- app/src/main/java/com/joi/school/fitness/MainActivity.java
+++ app/src/main/java/com/joi/school/fitness/MainActivity.java
@@ -2,77 +2,106 @@ package com.joi.school.fitness;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.StringDef;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
 
+import com.joi.school.fitness.home.CoreFragment;
+import com.joi.school.fitness.home.ForumFragment;
 import com.joi.school.fitness.home.HomeFragment;
-import com.joi.school.fitness.home.dummy.DummyContent;
+import com.joi.school.fitness.home.MineFragment;
 
-public class MainActivity extends AppCompatActivity implements HomeFragment.OnListFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    private Fragment mHomeFragment = new HomeFragment();
-    private Fragment mCurrentFragment = mHomeFragment;
+    public static final String FRAGMENT_TAG_HOME = "fragment_tag_home";
+    public static final String FRAGMENT_TAG_CORE = "fragment_tag_core";
+    public static final String FRAGMENT_TAG_FORUM = "fragment_tag_forum";
+    public static final String FRAGMENT_TAG_MINE = "fragment_tag_mine";
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    @StringDef({FRAGMENT_TAG_HOME, FRAGMENT_TAG_CORE, FRAGMENT_TAG_FORUM, FRAGMENT_TAG_MINE})
+    private @interface FragmentTag {
+    }
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    switchFragment(mHomeFragment);
-                    return true;
-                case R.id.navigation_dashboard:
-                    return true;
-                case R.id.navigation_notifications:
-                    return true;
-            }
-            return false;
-        }
-    };
+    private HomeFragment mHomeFragment;
+    private CoreFragment mCoreFragment;
+    private ForumFragment mForumFragment;
+    private MineFragment mMineFragment;
+    private Fragment mCurrentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setBottomNavigationBar();
+    }
+
+    private void setBottomNavigationBar() {
         BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-        initFragment();
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        switchFragment(FRAGMENT_TAG_HOME);
+                        return true;
+                    case R.id.navigation_core:
+                        switchFragment(FRAGMENT_TAG_CORE);
+                        return true;
+                    case R.id.navigation_forum:
+                        switchFragment(FRAGMENT_TAG_FORUM);
+                        return true;
+                    case R.id.navigation_mine:
+                        switchFragment(FRAGMENT_TAG_MINE);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
-    private void initFragment() {
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.add(R.id.fl_fragment_container, mHomeFragment);
-//        ft.hide(SecondFragment.getInstance());
-        ft.commit();
-    }
-
-    @Override
-    public void onListFragmentInteraction(DummyContent.DummyItem item) {
-        Log.d(TAG, "onListFragmentInteraction() called with: item = [" + item + "]");
-    }
-
-    private void switchFragment(Fragment targetFragment) {
+    private void switchFragment(@FragmentTag String fragmentTag) {
+        Fragment targetFragment;
+        switch (fragmentTag) {
+            case FRAGMENT_TAG_CORE:
+                if (mCoreFragment == null) {
+                    mCoreFragment = new CoreFragment();
+                }
+                targetFragment = mCoreFragment;
+                break;
+            case FRAGMENT_TAG_FORUM:
+                if (mForumFragment == null) {
+                    mForumFragment = new ForumFragment();
+                }
+                targetFragment = mForumFragment;
+                break;
+            case FRAGMENT_TAG_MINE:
+                if (mMineFragment == null) {
+                    mMineFragment = new MineFragment();
+                }
+                targetFragment = mMineFragment;
+                break;
+            case FRAGMENT_TAG_HOME:
+            default:
+                if (mHomeFragment == null) {
+                    mHomeFragment = new HomeFragment();
+                }
+                targetFragment = mHomeFragment;
+                break;
+        }
         FragmentTransaction transaction = getSupportFragmentManager()
                 .beginTransaction();
         if (!targetFragment.isAdded()) {
-            //第一次使用switchFragment()时currentFragment为null，所以要判断一下
             if (mCurrentFragment != null) {
                 transaction.hide(mCurrentFragment);
             }
             transaction.add(R.id.fl_fragment_container, targetFragment);
-
         } else {
             transaction
                     .hide(mCurrentFragment)
--- app/src/main/java/com/joi/school/fitness/home/MineFragment.java
+++ app/src/main/java/com/joi/school/fitness/home/MineFragment.java
@@ -1,31 +1,19 @@
 package com.joi.school.fitness.home;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.joi.school.fitness.R;
-
-public class HomeFragment extends Fragment {
+public class MineFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            recyclerView.setAdapter(null);
-        }
-        return view;
+        return null;
     }
 
     @Override
