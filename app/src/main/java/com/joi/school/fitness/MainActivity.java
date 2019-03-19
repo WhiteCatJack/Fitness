2f8fbb6cbd47c1dfe6af5bcf18b5a7d3c94a3409
ZekunWei <zekunwei@outlook.com>
2019-03-19 11:05:04 +0800

    basic

--- app/src/main/java/com/joi/school/fitness/MainActivity.java
+++ app/src/main/java/com/joi/school/fitness/MainActivity.java
@@ -3,13 +3,23 @@ package com.joi.school.fitness;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.joi.school.fitness.home.HomeFragment;
+import com.joi.school.fitness.home.dummy.DummyContent;
 
-    private TextView mTextMessage;
+public class MainActivity extends AppCompatActivity implements HomeFragment.OnListFragmentInteractionListener {
+
+    private static final String TAG = "MainActivity";
+
+    private Fragment mHomeFragment = new HomeFragment();
+    private Fragment mCurrentFragment = mHomeFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +28,11 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    switchFragment(mHomeFragment);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -36,9 +44,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        initFragment();
+    }
+
+    private void initFragment() {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.fl_fragment_container, mHomeFragment);
+//        ft.hide(SecondFragment.getInstance());
+        ft.commit();
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        Log.d(TAG, "onListFragmentInteraction() called with: item = [" + item + "]");
+    }
+
+    private void switchFragment(Fragment targetFragment) {
+        FragmentTransaction transaction = getSupportFragmentManager()
+                .beginTransaction();
+        if (!targetFragment.isAdded()) {
+            //第一次使用switchFragment()时currentFragment为null，所以要判断一下
+            if (mCurrentFragment != null) {
+                transaction.hide(mCurrentFragment);
+            }
+            transaction.add(R.id.fl_fragment_container, targetFragment);
+
+        } else {
+            transaction
+                    .hide(mCurrentFragment)
+                    .show(targetFragment);
+        }
+        mCurrentFragment = targetFragment;
+        transaction.commit();
     }
 
 }
